deee7e70a301c1d5523cb2358ac9bccafff0ea1c
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-09 13:42:07 +0100

    ajout de sass et d'axios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  // empecher Nuxt de préfixer les composants
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
+  // modules: [
+  //   "@pinia/nuxt"
+  // ],
+
+  css: ["@/scss/main.scss"],
+
+  // runtimeConfig: {
+  //   public: {
+  //     apiRecetteUrl: "",
+  //   },
+  // },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/scss/foundations/_variables.scss";
+                          @import "@/scss/foundations/_mixins.scss";
+          `,
+        },
+      },
+    },
+  },
+});
