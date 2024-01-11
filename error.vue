a39818bd75d1edec1dfcf2fee188bb98068d7eee
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-11 01:35:50 +0100

    création d'une page erreur 404

---
+++ error.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class="error">
+        <h1>Erreur {{ error.statusCode }}</h1>
+        <p>{{ error.statusMessage }}</p>
+    
+        <myButton class="error__bouton" lien="/">Retourner à l'accueil</myButton>
+    </main>
+</template>
+
+<style lang="scss">
+.error {
+    text-align: center;
+  
+    > * {
+        margin: 40px auto;
+    }
+
+    &__bouton{
+        width: fit-content;
+    }
+}
+</style>
+
+<script setup>
+const props = defineProps({
+  error: Object,
+});
+</script>
+  
