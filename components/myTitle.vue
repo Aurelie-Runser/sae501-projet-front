5fb1455e036b8c18d95d34d67d22ef66f8cda3cd
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-11 23:53:42 +0100

    création d'un composant myTitlte pour les titres des pages

---
+++ components/myTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="my_title">
+        <h1 class="my_title__texte"><slot class="my_title__texte-valeur"/></h1>
+    </div>
+</template>
+
+<style lang="scss">
+.my_title{
+    border: 2px solid transparent;
+    background: url("/img/montre.jpg");
+    margin: -100px;
+    margin-bottom: $m-big;
+    background-position: 50% 25%;
+
+    &__texte{
+        color: $color-white;
+    }
+}
+</style>
