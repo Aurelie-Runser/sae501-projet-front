3c1d3cfbcc35c0f1b264a892fceaccb46911ff96
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-01-11 15:01:28 +0100

    suppressions des commentaires inutils

--- app.vue
+++ app.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>
-    <NuxtLayout/>
-  </div>
+    <div>
+        <NuxtLayout/>
+    </div>
 </template>
 
 <script setup>
 const store = useGlobalStore()
 
 onMounted(() => {
-  // fonction pour vérifié si l'utilisateur était connecté à sa dernière visite
-  const token = localStorage.getItem("Token de Connection")
-  store.setToken(JSON.parse(token))
-  
-  // // fonction pour inialiser le panier en fonction des données enregistrées en local par rapport à la dernière visite
-  // const token = localStorage.getItem("Token de Connection")
-  // store.setToken(JSON.parse(token))
+    // fonction pour vérifié si l'utilisateur était connecté à sa dernière visite
+    const token = localStorage.getItem("Token de Connection")
+    store.setToken(JSON.parse(token))
 })
 </script>
