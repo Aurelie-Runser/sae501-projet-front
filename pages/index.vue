ee21a16e571b92586f085be8483e16227da44004
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2024-03-21 18:36:01 +0100

    format mobile index

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
     <main class="accueil">
         <section class="accueil__section">
-            <h1 class="accueil__titre">Créez vos propres montres sur mesure</h1>
+            <h1 class="accueil__section--titre">Créez vos propres montres sur mesure</h1>
 
-            <div class="accueil__boutons">
+            <div class="accueil__section--boutons">
                 <myButton lien="/montre">Voir les Montres</myButton>
                 <myButton v-if="!store.token" lien="/login" color="white">Login</myButton>
                 <myButton v-if="store.token" lien="/compte" color="white">Mon Compte</myButton>
@@ -14,22 +14,53 @@
 
 <style lang="scss">
 .accueil{
-    height: 90vh;
-    overflow: hidden;
-    background-image: url("@/img/montre.jpg");    
-    background-position: top center;
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
 
-    &__titre{
-        color: $color-white;
+    &::before{
+        content: "";
+        z-index: -1;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        background-image: url("@/img/montre.jpg");  
+        background-position: top center;
+        filter: brightness(80%);
     }
 
-    &__boutons{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-evenly;
-        gap: $m-litle;
+    &__section{
         width: 100%;
-        margin-top: 150px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: $m-big;
+    
+        &--titre{
+            color: $color-white;
+            margin: 0;
+        }
+    
+        &--boutons{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: $m-litle;
+        }
+    }
+
+    @include medium{
+        &__section{
+
+            &--boutons{
+                gap: $m-big;
+            }
+        }
     }
 }
 </style>
